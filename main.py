54b5d3d292d5437c862e3dbc1ff67d3a069f4d3b
nminhkhoi0818 <nminhkhoi0818@gmail.com>
2023-04-07 17:06:06 +0700

    Added input function and create algo.py

--- main.py
+++ main.py
@@ -1,55 +1,29 @@
-def PL_RESOLUTION(KB, alpha):
-    clauses = KB.copy()
-    clauses.add((alpha,))  # add negation of alpha to clauses
+from algo import PL_RESOLUTION
+
+#Read input
+def read_input():
+    f = open("PL-Resolution\input.txt", "r")
+    line = f.readline() #Get alpha
+    alpha = ""
+    for w in line.split():
+        if w != "\n":
+            alpha += w
+    if alpha[0] == "-":
+        alpha = alpha[1:]
+    else:
+        alpha = "-" + alpha
+    num = f.readline()
+    KB = set()
     while True:
-        new = set()
-        pairs = [(ci, cj) for ci in clauses for cj in clauses if ci != cj]
-        for ci, cj in pairs:
-            resolvents, check = resolve(ci, cj, clauses)
-            if check == True:
-                return True
-            new.update(resolvents)
-        print(len(new) + len(clauses))
-        print(new)
-        print(clauses)
-        if new.issubset(clauses):
-            return False
-        clauses.update(new)
-
-
-def check_valid(resolvent):
-    for el1 in resolvent:
-        for el2 in resolvent:
-            if el1 == "~" or el2 == "~" + el1:
-                return False
-    return True
-
-def check_in_clause(resolvent, clauses):
-    for cl in clauses:
-        if resolvent == set(cl):
-            return False
-    return True
+        line = f.readline()
+        if not line:
+            break
+        temp_list = []
+        for w in line.split():
+            if w != "OR":
+                temp_list.append(w)
+        KB.add(tuple(temp_list))
+    result = PL_RESOLUTION(KB, str(alpha))
+    print(result)
         
-
-def resolve(ci, cj, clauses):
-    resolvents = set()
-    for di in ci:
-        for dj in cj:
-            if di == "~" + dj or dj == "~" + di:  # check if literals are complementary
-                resolvent = (set(ci) | set(cj)) - {di, dj}
-                if not resolvent:  # check for empty clause
-                    return (), True
-                if check_valid(resolvent) == True and check_in_clause(set(tuple(resolvent)), clauses) == True and set(tuple(resolvent)) not in resolvents:
-                    resolvents.add(tuple(resolvent))
-    return resolvents, False
-
-# Example knowledge base and query
-KB = {('~A', 'B'), ('~C', 'B'), ('A', 'C', '~B'), ('~B',)}
-
-alpha = "A"
-
-# Check if KB entails alpha
-result = PL_RESOLUTION(KB, alpha)
-
-# Expected output: True
-print(result)
+read_input()
